ec17a218b607a0145490fa3377cb0a6f8bb9fff3
hemanshu-jatt <118927835+hemanshu-jatt@users.noreply.github.com>
2024-06-28 20:56:14 +0530

    Add files via upload

---
+++ password generator/script/scripts.js
@@ -0,0 +1,25 @@
+const passwordBox = document.getElementById("Password")
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbol = "!@#$%^&*()_+{}|:\"<>?";
+const allChars = upperCase+lowerCase+numbers+symbol;
+
+
+function createPass(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+function copy(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
